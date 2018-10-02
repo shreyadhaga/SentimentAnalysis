1b65c1b336e5183abe9fa6947b95b823f561dac8
ShreyaDhaga <shreyadhaga@gmail.com>
2018-10-03 00:17:42 +0530

    2

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -4,15 +4,41 @@ from tweepy import Stream
 
 import twitter_credentials
 
-class StdOutListener(StreamListener):
+class TwitterSreamer():
+   """
+    Class for streaming and processing live tweets
+   """
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authentication and connection to Twitter Sreaming API.
+        listener = StdOutListener()
+        auth =  OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
+    
+        stream = Stream(auth, listener)
 
+        stream.filter(track=hash_tag_list)
+        
+class StdOutListener(StreamListener):
+    """
+    This is a basic listner class that just prints recieved tweets stdout
+    """
+    def __init__(self,fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+        
     def on_data(self, data):
-        print(data)
-        return True
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on data: %s" %str(e))
+    
     def on_error(self, status):
         print(status)
 
-if name__== "__main__":
-    listener = StdOutListener()
-    auth =  OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_SECRET)
+if __name__== "__main__":
+
+    hash_tag_list = ["AIB"]
+
     
