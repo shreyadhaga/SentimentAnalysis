ea742044787b193151ec9cf83ca82c42f48784ee
ShreyaDhaga <shreyadhaga@gmail.com>
2018-10-03 01:05:57 +0530

    3

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -4,19 +4,22 @@ from tweepy import Stream
 
 import twitter_credentials
 
-class TwitterSreamer():
-   """
+class TwitterStreamer():
+    """
     Class for streaming and processing live tweets
    """
+    def __init__(self):
+        pass
+    
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter authentication and connection to Twitter Sreaming API.
-        listener = StdOutListener()
+        listener = StdOutListener(fetched_tweets_filename)
         auth =  OAuthHandler(twitter_credentials.CONSUMER_KEY,twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN,twitter_credentials.ACCESS_TOKEN_SECRET)
     
         stream = Stream(auth, listener)
 
-        stream.filter(track=hash_tag_list)
+        stream.filter(track = hash_tag_list)
         
 class StdOutListener(StreamListener):
     """
@@ -33,6 +36,7 @@ class StdOutListener(StreamListener):
             return True
         except BaseException as e:
             print("Error on data: %s" %str(e))
+        return True
     
     def on_error(self, status):
         print(status)
@@ -40,5 +44,9 @@ class StdOutListener(StreamListener):
 if __name__== "__main__":
 
     hash_tag_list = ["AIB"]
+    fetched_tweets_filename = "Tweets.txt"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
 
     
